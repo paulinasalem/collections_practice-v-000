bc2530c87b864e4ab1901a59007b7240ab158dc1
Paulina Salem <paulinasalem@yahoo.com>
2018-05-01 00:35:20 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -12,3 +12,14 @@ def sort_array_desc(array)
     end
   end
 end
+def sort_array_char_count(array)
+array.sort do |a,b|
+  if a.length==b.length
+    0
+  elsif a.length>b.length
+    1
+  elsif a.length<b.length
+    -1
+  end
+end
+end
